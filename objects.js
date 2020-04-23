974e58657e88377471414e12d72109f0af1e69d9
Dmitry Sychev <axiumone@gmail.com>
2020-04-23 15:28:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist() {
 playlist["Phil Ochs"] = "Here's to the State of Mississippi"
 }
 
-function removeFromPlaylist(playlist, Alison) {
+function removeFromPlaylist(playlist, Slowdive) {
   delete playlist.Alison;
 }
